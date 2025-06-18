d34b74365c10c2d75e76d7ca418145e80556eab0
Justin Hart <hart@cs.utexas.edu>
2025-06-18 12:59:29 -0500

    Adding line follower exercises.

---
+++ ex9/ex9_2/ex9_2.ino
@@ -0,0 +1,34 @@
+#include <BnrOneAPlus.h>
+#include <SPI.h>
+
+#define SSPIN 2
+
+BnrOneAPlus one;
+
+class LineFollowerInterface {
+public:
+  LineFollowerInterface() {}
+
+  void readLineFollower() {
+  }
+
+  void printSerial() {
+    
+  }
+
+protected:
+  int _adcVals[8];
+};
+
+LineFollowerInterface lineFollowerInterface;
+
+void setup() {
+  Serial.begin(115200);
+  one.spiConnect(SSPIN);
+  one.stop();
+}
+
+void loop() {
+  lineFollowerInterface.readLineFollower();
+  lineFollowerInterface.printSerial();
+}
